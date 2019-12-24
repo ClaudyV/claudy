9f0433f922fc3c5c090120092996e2435f084960
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2019-12-24 18:43:30 +0800

    Updates

---
+++ precache-manifest.7bafe2542ad2ab44522b2a14a0a5b638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10d13269cf9988e2e82013d2042a990",
+    "url": "/claudy/index.html"
+  },
+  {
+    "revision": "6871536b43ae9594b587",
+    "url": "/claudy/static/css/main.aca90e47.chunk.css"
+  },
+  {
+    "revision": "3df11086bd414518efac",
+    "url": "/claudy/static/js/2.44385c03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/claudy/static/js/2.44385c03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6871536b43ae9594b587",
+    "url": "/claudy/static/js/main.b08aa8f1.chunk.js"
+  },
+  {
+    "revision": "98d6fb5dab929a69cedd",
+    "url": "/claudy/static/js/runtime-main.5c6f36ce.js"
+  }
+]);
