bfc7e7e793b794543700b9c846abe3988265519f
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2020-03-15 14:11:54 +0800

    timeline and about modified

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -15,9 +15,19 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="timeline-centered">
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2><b>Web 前端開發工程師(Front-End Web Developer) at <a style={{color: "#2c98f0"}} href="https://www.moldex3d.com/en/"> Moldex3d </a> </b>  <span>(March 2020 - Present)</span></h2> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
