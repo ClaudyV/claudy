86cf99e64477e4a2f4eb61bdc04ae48eeef91017
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2023-02-08 22:45:19 +0800

    [feat][Update personal website]

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,39 +1,34 @@
 let portfoliData = {
-
-
-	"name": "Claude Vernet Michel", 
-	"role": "A Web Developer", 
-	"socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/claude-vernet-michel-10b1bb113/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/ClaudyV",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"Instagram",
-          "url":"https://www.instagram.com/claudybest/",
-          "className":"fa fa-twitter"
-        },
-        {
-          "name":"Facebook",
-          "url":"https://www.facebook.com/claudevernet.michel",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"Medium",
-          "url":"https://medium.com/@claudevernetmichel",
-          "className":"fa fa-github"
-        },
-      ],
-      "aboutMe1": "Since I was a little boy, I have always nourished a strong passion for understanding and learning the core technologies that drive the World Wide Web and Mobile applications. That passion pushes me to constantly explore new technologies."
-      "aboutMe2": "Besides technology, I like to write poems, work out and travel"
-
-
-
-
-}
+  name: "Claude Vernet Michel",
+  role: "A Web Developer",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/claude-vernet-michel-10b1bb113/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/ClaudyV",
+      className: "fa fa-github",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/claudybest/",
+      className: "fa fa-twitter",
+    },
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/claudevernet.michel",
+      className: "fa fa-github",
+    },
+    {
+      name: "Medium",
+      url: "https://medium.com/@claudevernetmichel",
+      className: "fa fa-github",
+    },
+  ],
+  aboutMe1:
+    "Since I was a little kid, I have always nourished a strong passion for understanding and learning the core technologies that drive the World Wide Web and Mobile applications. That passion pushes me to constantly explore new technologies.",
+  aboutMe2: "Besides technology, I like to write poems, work out and travel",
+};
