539125bad95b6a5a2a13ae99768ed13f28233f1e
Claude Vernet Michel <claudevernetmichel22@gmail.com>
2023-03-15 19:52:34 +0800

    [feat][Added projects section]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Timeline from './components/timeline'
-import Skill from './components/skills'
-
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Introduction from "./components/introduction";
+import About from "./components/about";
+import Timeline from "./components/timeline";
+import Skill from "./components/skills";
+import Projects from "./components/projects";
 
 class App extends Component {
   render() {
     return (
       <div id="claudy-page">
         <div id="container-wrap">
-         	<Sidebar></Sidebar>
-				<div id="claudy-main">
-					<Introduction></Introduction>
-					<About></About>
-					<Skill></Skill>
-					<Timeline></Timeline>
-          	</div>
-      	</div>
+          <Sidebar></Sidebar>
+          <div id="claudy-main">
+            <Introduction></Introduction>
+            <About></About>
+            <Skill></Skill>
+            <Timeline></Timeline>
+            <Projects></Projects>
+          </div>
+        </div>
       </div>
     );
   }
